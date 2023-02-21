a46dda36b2199841866e0f3ae3976bc659d6c35a
Natashya <natasha1dikola@gmail.com>
2023-02-21 11:29:24 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Print _putchar
  * Return: Always 0
  */
-
 int main(void)
-
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -19,5 +16,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-
-}}
+}
