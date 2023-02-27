27f4ff6a84af7f76112ea536098a88f5e00a8c03
Natashya <natasha1dikola@gmail.com>
2023-02-27 11:51:54 -0800

    reset-98

--- 0x05-pointers_arrays_string/0-reset_to_98.c
+++ 0x05-pointers_arrays_string/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Owned By Bwave/Bright
-*  Return: 0 is success
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
 */
 
 void reset_to_98(int *n)
 {
+
 	*n = 98;
+
 }
